ba84187f7285dde44358195064dd0eeb1d4778c5
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-03-24 09:31:39 +0300

    initial commit

---
+++ src/main/java/ru/nidecker/liderTestTask/LiderTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.nidecker.liderTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiderTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiderTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nidecker/liderTestTask/LiderTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nidecker.liderTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiderTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
