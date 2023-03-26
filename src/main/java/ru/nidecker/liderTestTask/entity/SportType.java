dcf79d37bb49a04b191a31fa87094c829f36e1fa
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-03-26 13:39:30 +0300

    created sport type entity

--- src/main/java/ru/nidecker/liderTestTask/entity/SportType.java
+++ src/main/java/ru/nidecker/liderTestTask/entity/SportType.java
@@ -1,37 +1,28 @@
 package ru.nidecker.liderTestTask.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.Hibernate;
 
-import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
 @NoArgsConstructor
-public class Team extends BaseEntityWithDate {
+public class SportType extends BaseEntity {
 
-    @NotNull
-    @ManyToOne
-    private SportType sportType;
-
-    public Team(String name, LocalDate date, SportType sportType) {
-        super(name, date);
-        this.sportType = sportType;
+    public SportType(String name) {
+        super(name);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Team team = (Team) o;
-        return getId() != null && Objects.equals(getId(), team.getId());
+        SportType sportType = (SportType) o;
+        return getId() != null && Objects.equals(getId(), sportType.getId());
     }
 
     @Override
