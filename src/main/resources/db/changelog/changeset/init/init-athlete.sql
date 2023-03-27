230f4f3d9f330a148050e6e207d31d31a79aeaaa
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-03-27 15:58:50 +0300

    code refactored

--- src/main/resources/db/changelog/changeset/init/init-athlete.sql
+++ src/main/resources/db/changelog/changeset/init/init-athlete.sql
@@ -1,40 +1,40 @@
 -- basketball
 insert into athlete (date, name, last_name, patronymic, position, team_id)
 -- golden state warriors
-VALUES ('14.03.1988', 'Stephen', 'Curry', '', 'PG', 1),
-       ('04.03.1990', 'Draymond', 'Green', '', 'PF', 1),
-       ('08.02.1990', 'Klay', 'Thompson', '', 'SG', 1),
-       ('19.06.1999', 'Jordan', 'Poole', '', 'SG', 1),
+VALUES ('03-14-1988', 'Stephen', 'Curry', '', 'PG', 1),
+       ('03-04-1990', 'Draymond', 'Green', '', 'PF', 1),
+       ('02-08-1990', 'Klay', 'Thompson', '', 'SG', 1),
+       ('06-19-1999', 'Jordan', 'Poole', '', 'SG', 1),
 
 -- los angeles lakers
-       ('11.03.1993', 'Anthony', 'Davis', '', 'PF', 2),
-       ('30.12.1984', 'Lebron', 'James', '', 'SF', 2),
-       ('29.05.1998', 'Austin', 'Reaves', '', 'SG', 2),
+       ('03-11-1993', 'Anthony', 'Davis', '', 'PF', 2),
+       ('12-30-1984', 'Lebron', 'James', '', 'SF', 2),
+       ('05-29-1998', 'Austin', 'Reaves', '', 'SG', 2),
 
 -- dallas mavericks
-       ('23.03.1992', 'Kyrie', 'Irving', '', 'PG', 4),
-       ('28.02.1999', 'Luka', 'Doncic', '', 'PG', 4);
+       ('03-23-1992', 'Kyrie', 'Irving', '', 'PG', 4),
+       ('02-28-1999', 'Luka', 'Doncic', '', 'PG', 4);
 
 -- american football
 INSERT INTO athlete (date, name, last_name, patronymic, position, team_id)
 -- miami dolphins
-VALUES ('15.02.1997', 'Myles', 'Gaskin', '', 'RB', 5),
-       ('25.04.1996', 'John', 'Lovett', '', 'F', 5),
-       ('01.03.1994', 'Tyreek', 'Hill', '', 'WR', 5),
+VALUES ('02-15-1997', 'Myles', 'Gaskin', '', 'RB', 5),
+       ('04-25-1996', 'John', 'Lovett', '', 'F', 5),
+       ('03-01-1994', 'Tyreek', 'Hill', '', 'WR', 5),
 
 -- arizona cardinals
-       ('15.04.1995', 'Darrel', 'Williams', '', 'RB', 8),
-       ('02.11.1994', 'Corey', 'Clement', '', 'RB', 8),
-       ('09.06.2000', 'Rondale', 'Moore', '', 'WR', 8);
+       ('04-15-1995', 'Darrel', 'Williams', '', 'RB', 8),
+       ('11-02-1994', 'Corey', 'Clement', '', 'RB', 8),
+       ('06-09-2000', 'Rondale', 'Moore', '', 'WR', 8);
 
 -- hockey
 INSERT INTO athlete (date, name, last_name, patronymic, position, team_id)
 -- торпедо
-VALUES ('17.10.1999', 'Николай', 'Коваленко', 'Андреевич', 'Нападающий', 10),
-       ('24.07.1996', 'Даниил', 'Бокун', 'Владимирович', 'Защитник', 10),
-       ('13.07.1999', 'Сергей', 'Гончарук', 'Иванович', 'Нападающий', 10),
+VALUES ('10-17-1999', 'Nikolai', 'Kovalenko', 'Andreevich', 'Forward', 10),
+       ('07-24-1996', 'Daniil', 'Bokun', 'Vladimirovich', 'Defender', 10),
+       ('07-13-1999', 'Sergei', 'Goncharuk', 'Ivanovich', 'Forward', 10),
 
 -- авангард
-       ('07.03.2001', 'Сергей', 'Алхимов', 'Владимирович', 'Нападающий', 11),
-       ('29.07.1986', 'Антон', 'Белов', 'Сергеевич', 'Защитник', 11),
-       ('15.03.2001', 'Арсений', 'Грицюк', 'Сергеевич', 'Нападающий', 11);
+       ('03-07-2001', 'Sergei', 'Alhimov', 'Vladimirovich', 'Forward', 11),
+       ('07-29-1986', 'Anton', 'Belov', 'Sergeevich', 'Defender', 11),
+       ('03-15-2001', 'Arseniy', 'Gricuk', 'Sergeevich', 'Forward', 11);
